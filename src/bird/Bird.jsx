470fdc4b833ae5c4d8e50f22f7b90a39ec678464
Karin Knutsson <karinmiriam@gmail.com>
2025-12-09 08:09:48 +0100

    added legs

--- src/bird/Bird.jsx
+++ src/bird/Bird.jsx
@@ -5,6 +5,7 @@ const sphereGeometry = new THREE.IcosahedronGeometry(1, 30);
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const coneGeometry = new THREE.ConeGeometry(1, 1, 4, 1);
 const birdMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff" });
+const legMaterial = new THREE.MeshStandardMaterial({ color: "#fc5454" });
 
 export default function Bird({ position }) {
   return (
@@ -24,6 +25,68 @@ export default function Bird({ position }) {
         scale={[0.4, 1, 0.4]}
         rotation={[Math.PI * 0.6, 0, 0]}
       />
+
+      {/* Left leg */}
+      <group position={[0.4, 0.1, 0]}>
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0, -1.3, 0]}
+          scale={[0.1, 1, 0.1]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0.17, -1.8, 0.14]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, Math.PI * 0.25, 0]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0, -1.8, 0.15]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, 0, 0]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[-0.17, -1.8, 0.14]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, -Math.PI * 0.25, 0]}
+        />
+      </group>
+
+      {/* Right leg */}
+      <group position={[-0.4, 0.1, 0]}>
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0, -1.3, 0]}
+          scale={[0.1, 1, 0.1]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0.17, -1.8, 0.14]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, Math.PI * 0.25, 0]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[0, -1.8, 0.15]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, 0, 0]}
+        />
+        <mesh
+          geometry={boxGeometry}
+          material={legMaterial}
+          position={[-0.17, -1.8, 0.14]}
+          scale={[0.1, 0.1, 0.4]}
+          rotation={[0.1, -Math.PI * 0.25, 0]}
+        />
+      </group>
     </group>
   );
 }
