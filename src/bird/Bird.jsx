a0c5b6fd056ffd4cecfcf63e3efe9e865fd94e4b
Karin Knutsson <karinmiriam@gmail.com>
2025-12-29 15:24:13 +0100

    jumping works in the 4 zones

--- src/bird/Bird.jsx
+++ src/bird/Bird.jsx
@@ -1,7 +1,7 @@
 import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 import { useKeyboardControls } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useMemo } from "react";
 import useGame from "../stores/useGame";
 
 /**
@@ -32,7 +32,7 @@ export default function Bird({ position }) {
   const birdRef = useRef();
 
   const birdDirection = useRef("downLeft");
-  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const [subscribeKeys] = useKeyboardControls();
   let isJumping = false;
 
   const start = useGame((state) => state.start);
@@ -42,6 +42,43 @@ export default function Bird({ position }) {
   /**
    * Jump functionality
    */
+  // Movement directions
+  const movement = useMemo(() => {
+    switch (cameraPosition) {
+      case 0:
+        return {
+          upLeft: { x: -0.29, z: 0 },
+          upRight: { x: 0, z: -0.29 },
+          downLeft: { x: 0, z: 0.39 },
+          downRight: { x: 0.39, z: 0 },
+        };
+      case 1:
+        return {
+          upLeft: { x: 0, z: -0.29 },
+          upRight: { x: 0.29, z: 0 },
+          downLeft: { x: -0.39, z: 0 },
+          downRight: { x: 0, z: 0.39 },
+        };
+      case 2:
+        return {
+          upLeft: { x: 0.29, z: 0 },
+          upRight: { x: 0, z: 0.29 },
+          downLeft: { x: 0, z: -0.39 },
+          downRight: { x: -0.39, z: 0 },
+        };
+      case 3:
+        return {
+          upLeft: { x: 0, z: 0.29 },
+          upRight: { x: -0.29, z: 0 },
+          downLeft: { x: 0.39, z: 0 },
+          downRight: { x: 0, z: -0.39 },
+        };
+    }
+  }, [cameraPosition]);
+
+  const movementRef = useRef(movement);
+  movementRef.current = movement;
+
   // Lock / unlock jump
   const setIsJumping = (value, ms = 800) => {
     if (value) {
@@ -53,49 +90,26 @@ export default function Bird({ position }) {
     }
   };
 
-  // Jump down left
-  const jumpDownLeft = () => {
-    setIsJumping(true);
-    birdRef.current.applyImpulse({ x: 0, y: 1.2, z: 0.39 });
-
-    if (birdDirection.current === "downRight") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.008, z: 0 });
-    } else if (birdDirection.current === "upRight") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.016, z: 0 });
-    } else if (birdDirection.current === "upLeft") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.008, z: 0 });
-    }
-
-    birdDirection.current = "downLeft";
-    setIsJumping(false);
-  };
-
-  // Jump down right
-  const jumpDownRight = () => {
-    setIsJumping(true);
-    birdRef.current.applyImpulse({ x: 0.39, y: 1.2, z: 0 });
-
-    if (birdDirection.current === "downLeft") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.008, z: 0 });
-    } else if (birdDirection.current === "upRight") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.008, z: 0 });
-    } else if (birdDirection.current === "upLeft") {
-      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.016, z: 0 });
-    }
-
-    birdDirection.current = "downRight";
-    setIsJumping(false);
-  };
-
   // Jump up left
   const jumpUpLeft = () => {
+    // console.log(movementRef.current.upLeft);
     setIsJumping(true);
     const position = birdRef.current.translation();
 
-    if (position.x > 0.1) {
-      birdRef.current.applyImpulse({ x: -0.29, y: 2.4, z: 0 });
+    // If bird is on edge cube, jump downward
+    if (position.x < 0.3 && position.x > -0.3) {
+      birdRef.current.applyImpulse({
+        x: movementRef.current.upLeft.x * 1.4,
+        y: 1.2,
+        z: movementRef.current.upLeft.z * 1.4,
+      });
     } else {
-      birdRef.current.applyImpulse({ x: -0.4, y: 1.2, z: 0 });
+      // On non-edge cubes, jump upward
+      birdRef.current.applyImpulse({
+        x: movementRef.current.upLeft.x,
+        y: 2.4,
+        z: movementRef.current.upLeft.z,
+      });
     }
 
     if (birdDirection.current === "downLeft") {
@@ -112,15 +126,25 @@ export default function Bird({ position }) {
 
   // Jump up right
   const jumpUpRight = () => {
+    // console.log(movementRef.current.upRight);
     setIsJumping(true);
     const position = birdRef.current.translation();
 
-    if (position.z > 0.1) {
-      birdRef.current.applyImpulse({ x: 0, y: 2.4, z: -0.29 });
+    // If bird is on edge cube, jump downward
+    if (position.z < 0.3 && position.z > -0.3) {
+      birdRef.current.applyImpulse({
+        x: movementRef.current.upRight.x * 1.4,
+        y: 1.2,
+        z: movementRef.current.upRight.z * 1.4,
+      });
     } else {
-      birdRef.current.applyImpulse({ x: 0, y: 1.2, z: -0.4 });
+      // On non-edge cubes, jump upward
+      birdRef.current.applyImpulse({
+        x: movementRef.current.upRight.x,
+        y: 2.4,
+        z: movementRef.current.upRight.z,
+      });
     }
-
     if (birdDirection.current === "downLeft") {
       birdRef.current.applyTorqueImpulse({ x: 0, y: 0.012, z: 0 });
     } else if (birdDirection.current === "downRight") {
@@ -133,6 +157,51 @@ export default function Bird({ position }) {
     setIsJumping(false, 1150);
   };
 
+  // Jump down left
+  const jumpDownLeft = () => {
+    // console.log(movementRef.current.downLeft);
+    setIsJumping(true);
+    birdRef.current.applyImpulse({
+      x: movementRef.current.downLeft.x,
+      y: 1.2,
+      z: movementRef.current.downLeft.z,
+    });
+
+    if (birdDirection.current === "downRight") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.008, z: 0 });
+    } else if (birdDirection.current === "upRight") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.016, z: 0 });
+    } else if (birdDirection.current === "upLeft") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.008, z: 0 });
+    }
+
+    birdDirection.current = "downLeft";
+    setIsJumping(false);
+  };
+
+  // Jump down right
+  const jumpDownRight = () => {
+    // console.log(movementRef.current.downRight);
+    setIsJumping(true);
+    birdRef.current.applyImpulse({
+      x: movementRef.current.downRight.x,
+      y: 1.2,
+      z: movementRef.current.downRight.z,
+    });
+
+    if (birdDirection.current === "downLeft") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.008, z: 0 });
+    } else if (birdDirection.current === "upRight") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: -0.008, z: 0 });
+    } else if (birdDirection.current === "upLeft") {
+      birdRef.current.applyTorqueImpulse({ x: 0, y: 0.016, z: 0 });
+    }
+
+    birdDirection.current = "downRight";
+    setIsJumping(false);
+  };
+
+  // Subscribe to jump keys
   useEffect(() => {
     const unsubscribeAny = subscribeKeys(() => {
       start();
@@ -175,6 +244,9 @@ export default function Bird({ position }) {
     };
   }, []);
 
+  /**
+   * Bird collision
+   */
   const birdCollision = () => {
     const position = birdRef.current.translation();
     console.log("x: " + position.x, ", z: " + position.z);
